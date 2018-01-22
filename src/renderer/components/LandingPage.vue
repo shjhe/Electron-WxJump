925edc70ef59aa181e0665ba513f04daa62bf16e
何仕敬 <hesj1139@13322ty.com>
2018-01-22 19:09:05 +0800

    连接状态组件构建

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,40 +1,33 @@
 <template>
   <div id="wrapper">
-    <main>
-      <div class="logger-wrapper" ref="logger">
-        <p v-for="log in loggers" :key="log">{{log}}</p>
-      </div>
-    </main>
   </div>
 </template>
 
 <script>
-  import SystemInformation from './LandingPage/SystemInformation'
   import shell from 'shelljs'
   export default {
-    name: 'landing-page',
-    components: { SystemInformation },
     data () {
       return {
         loggers: []
       }
     },
     created () {
-      // da
+      this.initState()
     },
     methods: {
-      addLog () {
-        // console.log(this.loggers)
-        this.loggers.push(`>>>>>>>>这是第${this.loggers.length + 1}条信息`)
-        this.$nextTick(() => {
-          this.$refs.logger.scrollTop = this.$refs.logger.scrollHeight + 20
-        })
-        console.log(this.$refs.logger.sc)
-      },
-      closeWindow () {
-        console.log('12121')
-        shell.exec('node -v', function(...arg) {
-          console.log(arg)
+      initState () {
+        // 查看是否连接手机并正常安装驱动
+        Shell.exec('adb devices', (code, stdout, stderr) => {
+          let flag = false
+          debugger
+          if (code === 0) {
+            const reg = /.+\s+device\b/g
+            let mathArr = stdout.match(reg) || []
+            if (mathArr.length) {
+              flag = true
+            }
+          }
+          this.state.isConnect = flag
         })
       }
     }
