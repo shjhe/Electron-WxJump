ac6da61e11fff3a3fd085bc3c821cf06f55317ca
shjhe <568675732@qq.com>
2018-02-08 18:42:11 +0800

    修改标题及添加缓冲页面

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,12 +1,27 @@
 <template>
-  <div id="app">
+  <div id="app" v-loading.fullscreen.lock="loading">
     <router-view></router-view>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'test1'
+    name: 'App',
+    data () {
+      return {
+        loading: true
+      }
+    },
+    created () {
+      this.loading = true
+    },
+    mounted () {
+      this.$nextTick(_ => {
+        setTimeout(_ => {
+          this.loading = false
+        }, 2000)
+      })
+    }
   }
 </script>
 
---
+++ src/renderer/components/Index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home-page-wrapper">
+    <div class="logo-wrapper">
+      <img class="logo" src="/static/images/logo.jpg" alt="">
+    </div>
+    <div class="welcome">
+      欢迎使用微信跳一跳辅助软件
+    </div>
+    <div class="button-wrapper">
+      <el-button class="button" type="primary" @click="handleBegin">立即开始</el-button>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Welcome',
+    data () {
+      return {}
+    },
+    methods: {
+      handleBegin () {
+        this.$router.push('/jump')
+      }
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .home-page-wrapper{
+    text-align: center;
+    .logo-wrapper{
+      margin-top: 60px;
+      .logo{
+        width: 256px;
+      }
+    }
+    .welcome{
+      font-size: 24px;
+    }
+    .button-wrapper{
+      margin-top: 40px;
+      .button{
+        width: 200px;
+        font-size: 18px;
+      }
+    }
+  }
+</style>
